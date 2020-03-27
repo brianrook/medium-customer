e3cc1bf8add82b672bdf962e1ed53c7d76bf6110
brian <brian.rook@gmail.com>
2020-03-27 15:14:00 -0600

    initial commit

---
+++ src/test/java/com/brianrook/medium/customer/MediumCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianrook.medium.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
